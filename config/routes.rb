e4150ccc972ea04673fbe2d1fcc7b0e1f6d12f67
Oscar Jasklowski <oscarjasklowski@gmail.com>
2013-06-04 00:15:34 -0600

    Test new commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Omrails::Application.routes.draw do
+
+  get 'about' => 'pages#about'
+
+  get 'test' => 'pages#test'
+
+  root :to => 'pages#home'
+ 
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
